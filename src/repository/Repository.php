7d3a4479487ebaead27daa80e6e609de6afdad35
matows <mateusz.owsiak@student.pk.edu.pl>
2023-01-22 21:53:37 +0100

    Deleted comments

--- src/repository/Repository.php
+++ src/repository/Repository.php
@@ -5,7 +5,6 @@ require_once __DIR__.'/../../Database.php';
 class Repository
 {
     protected $database;
-//    zrobic singleton
     public function __construct()
     {
         $this->database = new Database();
