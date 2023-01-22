8eb6624d7b409b2466f56da2cd47e2393fe34da9
matows <mateusz.owsiak@student.pk.edu.pl>
2023-01-22 21:47:49 +0100

    Updated repository for activity

--- src/repository/ActivityRepository.php
+++ src/repository/ActivityRepository.php
@@ -5,6 +5,22 @@ require_once __DIR__.'/../models/Activity.php';
 require_once __DIR__.'/../models/Exercise.php';
 class ActivityRepository extends Repository
 {
+    public function getActivityByDate(int $ID_user,string $date){
+        $stmt = $this->database->connect()->prepare('
+        SELECT * FROM activities WHERE "ID_user"=:ID_user AND activity_date=:date
+        ');
+        $stmt->bindParam(":ID_user",$ID_user,PDO::PARAM_INT);
+        $stmt->bindParam(":date",$date,PDO::PARAM_STR);
+        $stmt->execute();
+        $activity = $stmt->fetch(PDO::FETCH_ASSOC);
+        if($activity == false)
+            return null;
+        return new Activity(
+            $activity['ID_user'],
+            $activity['activity_date'],
+            $activity['ID_activity']
+        );
+    }
     public function getActivity(int $ID_user,string $date){
         $stmt = $this->database->connect()->prepare('
         SELECT * FROM exercises LEFT OUTER JOIN public.activities a on exercises."ID_activity" = a."ID_activity"
